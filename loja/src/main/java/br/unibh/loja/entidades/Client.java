7eca013d2d7fbf8a6796021f8c3e3fbb044a2825
115218364 <115218364@ESTB8LI0613.animaedu.intranet>
2018-03-15 22:29:20 -0300

    COnfiguração e as tres primeiras classes

---
+++ loja/src/main/java/br/unibh/loja/entidades/Client.java
@@ -0,0 +1,100 @@
+package br.unibh.loja.entidades;
+
+import java.sql.Date;
+
+public class Client {
+	public Client() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", nome=" + nome + ", login=" + login + ", senha=" + senha + ", perfil=" + perfil
+				+ ", cpf=" + cpf + ", telefone=" + telefone + ", email=" + email + ", dataNascimento=" + dataNascimento
+				+ ", dataCadastro=" + dataCadastro + "]";
+	}
+	public Client(Long id, String nome, String login, String senha, String perfil, String cpf, String telefone,
+			String email, Date dataNascimento, Date dataCadastro) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.login = login;
+		this.senha = senha;
+		this.perfil = perfil;
+		this.cpf = cpf;
+		this.telefone = telefone;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.dataCadastro = dataCadastro;
+	}
+	private Long id;
+	private String nome;
+	private String login;
+	private String senha;
+	private String perfil;
+	private String cpf;
+	private String telefone;
+	private String email;
+	private Date dataNascimento;
+	private Date dataCadastro;
+}
